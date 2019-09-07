97df61bd5b60bd46fc4e82dbbbb3e8447a4fec70
chris <532797+thisguychris@users.noreply.github.com>
2019-09-08 01:56:10 +0800

    add fish_ssh_agent

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,3 +2,5 @@ alias fd=fdfind
 
 set -gx FZF_DEFAULT_COMMAND 'fd --hidden --exclude .git --exclude node_modules --type f --color=never'
 set -gx FZF_ALT_C_COMMAND 'fd --type d . --color=never'
+
+fish_ssh_agent
