cf36018c66808f028d850e58a19d2c2ff7705c8e
chris <532797+thisguychris@users.noreply.github.com>
2020-03-04 14:33:17 +0800

    double fnm declartion

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -54,7 +54,3 @@ if [ -f '/home/chris/google-cloud-sdk/path.fish.inc' ]; . '/home/chris/google-cl
 # fnm
 set PATH /home/chris/.fnm $PATH
 fnm env --multi | source
-
-# fnm
-set PATH /home/chris/.fnm $PATH
-fnm env --multi | source
