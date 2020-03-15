ccb6613d27cd24561bc8ba2bb607b1e98df8e9cb
chris <532797+thisguychris@users.noreply.github.com>
2020-03-16 00:00:00 +0800

    comment out

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -21,6 +21,12 @@ if type -q gpg
   set -x GPG_TTY (tty)
 end
 
+# set -x SSH_AUTH_SOCK $HOME/.ssh/agent.sock
+# ss -a | grep -q $SSH_AUTH_SOCK
+# if [ $status != 0 ]
+#   rm -f $SSH_AUTH_SOCK
+#   setsid nohup socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:$HOME/.ssh/wsl2-ssh-pageant.exe >/dev/null 2>&1 &
+# end
 # set DISPLAY variable for WSL2
 if uname -a | grep 'microsoft' >/dev/null
   set -gx DISPLAY (grep nameserver /etc/resolv.conf | awk '{print $2}'):0
