1db2baa8ebe2f297712eb9b018db2090738a97b3
chris <532797+thisguychris@users.noreply.github.com>
2020-03-17 03:45:20 +0800

    test bass

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -27,6 +27,9 @@ end
 #   rm -f $SSH_AUTH_SOCK
 #   setsid nohup socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:$HOME/.ssh/wsl2-ssh-pageant.exe >/dev/null 2>&1 &
 # end
+# bass source /home/chris/gpg.sh
+# /home/chris/bin/gpg-agent-relay.sh &
+
 # set DISPLAY variable for WSL2
 if uname -a | grep 'microsoft' >/dev/null
   set -gx DISPLAY (grep nameserver /etc/resolv.conf | awk '{print $2}'):0
