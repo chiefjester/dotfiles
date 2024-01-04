5e0bcd6674111285493745795831e69c73eedfe9
chris <532797+thisguychris@users.noreply.github.com>
2024-01-05 06:42:46 +0800

    add neovim plugins

---
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+return {
+  { "nvimdev/dashboard-nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "folke/persistence.nvim", enabled = false },
+}
