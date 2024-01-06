9e385aaf6c41d1a65f97a0f7298365ef023f6045
chris <532797+thisguychris@users.noreply.github.com>
2024-01-07 02:25:47 +0800

    use line for default cursor

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -46,6 +46,7 @@ function fish_hybrid_key_bindings --description \
     "Vi-style bindings that inherit emacs-style bindings in all modes"
     for mode in default insert visual
         fish_default_key_bindings -M $mode
+        set fish_cursor_default line
     end
     fish_vi_key_bindings --no-erase
 end
