765ba628e011096b1e41e8b197409fa04a605a05
chris <532797+thisguychris@users.noreply.github.com>
2024-01-07 02:10:17 +0800

    use a for window close

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -6,7 +6,7 @@
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 -- general enhancements
-vim.keymap.set("n", "<leader>q", ":silent q<cr>", { desc = "Close Window", silent = true })
+vim.keymap.set("n", "<leader>a", ":silent q<cr>", { desc = "Close Window", silent = true })
 vim.keymap.set("n", "<leader>z", ":silent update<cr>", { desc = "Save", silent = true })
 vim.keymap.set("n", "<leader>o", ":only<cr>", { desc = "Only", silent = true })
 vim.keymap.set("n", "<leader>gs", ":Git<cr>", { desc = "Git Status", silent = true })
