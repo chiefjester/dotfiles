450fecb86764c68bc9bcc96f2666596962340ada
chris <532797+thisguychris@users.noreply.github.com>
2024-02-03 23:10:40 +0800

    disable context
    - it still can be enabled by using <leader>ut
    - doesn't unload or remove the plugin
    - it only disables it on first load (which is what I want)

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -3,4 +3,11 @@ return {
   { "folke/noice.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
   { "folke/persistence.nvim", enabled = false },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    opts = {
+      -- still loads the plugin, but starts at disabled
+      enable = false,
+    },
+  },
 }
